e4776773d29b801810f566365acee654cff8c8fa
hir01 <glazunovden@yandex.ru>
2022-12-20 23:24:49 +0300

    add tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,14 @@ module.exports = {
     ident: 'postcss',
     syntax: 'postcss-scss',
     "plugins": {
+        // "postcss-import": {},
+        "tailwindcss": {},
         "postcss-preset-env": {
             "stage": 1,
             "features": {
                 "custom-properties": true
             }
-        }
+        },
+        "autoprefixer": {}
     }
 }
