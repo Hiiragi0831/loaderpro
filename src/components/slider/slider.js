6d5e494dbf7389e05c3080cde5c8dcbfd3c5d886
hir01 <glazunovden@yandex.ru>
2023-01-12 10:19:05 +0300

    fix

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -2,23 +2,25 @@ import Swiper, { Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css';
 
 export const slider = () => {
-  const logo = new Swiper('.slider__logo .swiper', {
-      modules: [Pagination],
-      spaceBetween: 7,
-      pagination: {
-          el: '.slider__logo .swiper-pagination',
-      },
-      breakpoints: {
-          // when window width is >= 320px
-          320: {
-              slidesPerView: 1,
-          },
+    if (document.querySelector(".slider__logo")) {
+        const logo = new Swiper('.slider__logo .swiper', {
+            modules: [Pagination],
+            spaceBetween: 7,
+            pagination: {
+                el: '.slider__logo .swiper-pagination',
+            },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 1,
+                },
 
-          1024: {
-              slidesPerView: 4,
-          }
-      }
-  });
+                1024: {
+                    slidesPerView: 4,
+                }
+            }
+        });
 
-  logo.init();
+        logo.init();
+    }
 }
