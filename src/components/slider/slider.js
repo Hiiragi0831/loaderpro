60e9a7d3ef34c5ee929641e1fae6d4e1f21ffee7
hir01 <glazunovden@yandex.ru>
2023-01-16 16:45:22 +0300

    fix

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,4 +1,4 @@
-import Swiper, { Pagination } from 'swiper';
+import Swiper, { Pagination, Grid } from 'swiper';
 import 'swiper/swiper-bundle.css';
 
 export const slider = () => {
@@ -22,5 +22,62 @@ export const slider = () => {
         });
 
         logo.init();
+    }
+
+    if (document.querySelector(".slider__logo-two")) {
+        const logoTwo = new Swiper('.slider__logo-two .swiper', {
+            modules: [Pagination, Grid],
+            pagination: {
+                el: '.slider__logo-two .swiper-pagination',
+            },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 2,
+                    grid: {
+                        rows: 3,
+                    },
+                },
+
+                1024: {
+                    slidesPerView: 5,
+                    slidesPerColumn: 3,
+                    grid: {
+                        rows: 3,
+                    },
+                },
+
+                1440: {
+                    slidesPerView: 7,
+                    grid: {
+                        rows: 3,
+                    },
+                }
+            }
+        });
+
+        logoTwo.init();
+    }
+
+    if (document.querySelector(".slider__popular")) {
+        const popular = new Swiper('.slider__popular .swiper', {
+            modules: [Pagination],
+            spaceBetween: 7,
+            pagination: {
+                el: '.slider__popular .swiper-pagination',
+            },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 1,
+                },
+
+                1024: {
+                    slidesPerView: 4,
+                }
+            }
+        });
+
+        popular.init();
     }
 }
